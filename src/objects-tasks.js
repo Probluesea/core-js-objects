d6ce4aed3ac62d9351a6adf410fd9f0cd20d9999
Probluesea <danil.kriw@gmail.com>
2024-03-15 14:20:18 +0500

    feat: complete makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -142,8 +142,21 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+  const entries = Object.entries(lettersObject);
+
+  for (let i = 0; i < entries.length; i += 1) {
+    const entry = entries[i];
+    const [letter, positions] = entry;
+
+    for (let j = 0; j < positions.length; j += 1) {
+      const position = positions[j];
+      result[position] = letter;
+    }
+  }
+
+  return result.join('');
 }
 
 /**
